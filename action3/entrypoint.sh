5a81d68df67638bceda12e9c9d98d3fd4d4a9744
rodrigociro <rodrigociro@hotmail.es>
2023-08-08 13:05:05 +0000

    uplaod files

--- action3/entrypoint.sh
+++ action3/entrypoint.sh
@@ -1,12 +1,1 @@
-#!/bin/sh -l
-echo "script from docker"
-echo $"El parametro introducido es: $1"
-if [ $1="yes" ]
-then
-  echo "Se ha introducido el argummento YES"
-elif [ $1="no"]
-then
-  echo "Se ha introducido el argummento NO"
-else
-  echo "no se ha introducido argumentos"
-fi
+CCC
