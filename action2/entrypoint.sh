2530322a3d0bddb994a099a4c56172d0fb7c4c77
rodrigociro <rodrigociro@hotmail.es>
2023-08-08 13:02:42 +0000

    upload files

--- action2/entrypoint.sh
+++ action2/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 echo "script from docker"
-
+echo $"El parametro introducido es: $1"
 if [ $1="yes" ]
 then
   echo "Se ha introducido el argummento YES"
-elif [ $1 = "no"]
+elif [ $1="no"]
 then
   echo "Se ha introducido el argummento NO"
 else
